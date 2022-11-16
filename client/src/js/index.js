84cd75abe2a43eb3fec919a2eafd6b9034fbdd56
Duder <thedudeabides1981@hotmail.com>
2022-11-16 00:55:31 -0800

    added GET and POST functions

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -13,10 +13,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../images/new-logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
-import { initdb } from "./database";
+
+import { getDb, initdb, postDb } from "./database";
+
 
 window.addEventListener('load', function () {
   initdb();
+
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
