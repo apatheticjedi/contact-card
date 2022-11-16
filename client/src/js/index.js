c3e7e4a9172e452df8b91ce04307870cea166fb7
Duder <thedudeabides1981@hotmail.com>
2022-11-15 23:57:04 -0800

    added webpack

---
+++ client/src/js/index.js
@@ -0,0 +1,21 @@
+// import modules
+import  "./form";
+import "./submit";
+
+// import CSS files
+import "../css/index.css"
+
+// import bootstrap
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import images
+import Logo from '../images/new-logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+
+window.addEventListener('load', function () {
+    document.getElementById('logo').src = Logo;
+    document.getElementById('bearThumbnail').src = Bear;
+    document.getElementById('dogThumbnail').src = Dog;
+  });
