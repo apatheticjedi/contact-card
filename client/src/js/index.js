f470340c6803066b159c6825674df266edfc8a8c
Duder <thedudeabides1981@hotmail.com>
2022-11-16 00:21:22 -0800

    added indexeddb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,5 @@
 // import modules
-import  "./form";
+import "./form";
 import "./submit";
 
 // import CSS files
@@ -13,9 +13,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../images/new-logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+import { initdb } from "./database";
 
 window.addEventListener('load', function () {
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
-  });
+  initdb();
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
