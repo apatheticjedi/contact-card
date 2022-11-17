9ade6d5392ee9396d359571b47aa9aee42b56974
Duder <thedudeabides1981@hotmail.com>
2022-11-16 17:17:07 -0800

    added install button

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -23,6 +23,24 @@ window.addEventListener('load', function () {
   document.getElementById('dogThumbnail').src = Dog;
 });
 
+// install button
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
+
+  installBtn.addEventListener('click', () => {
+    event.prompt();
+    installBtn.setAttribute('disabled', true);
+    installBtn.textContent = 'Installed!';
+  });
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+});
+
 // Form functionality
 const form = document.getElementById("formToggle");
 const newContactButton = document.getElementById("new-contact");
@@ -31,7 +49,7 @@ let profileId;
 
 newContactButton.addEventListener('click', event => {
   toggleForm()
-})
+});
 
 form.addEventListener('submit', event => {
   // Handle data
